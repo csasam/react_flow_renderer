17bc2934ba0b0f892d68477eacb2e372d984072e
csasam <amawian@gmail.com>
2022-10-06 22:24:29 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FactoryFlow from './components/FactoryFlow';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+     <Row>
+      <h1>Factory Flow</h1>
+     </Row>
+        
+    <Row>
+    <FactoryFlow />
+    </Row>
+    <div>
+      <Button>Create Square Node</Button>{' '}
+      <Button>Create Circle Node</Button>{' '}   
+      <Button>Create Triangle Node</Button>{' '}
     </div>
+      
+    </Container>
   );
 }
 
